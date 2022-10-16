8d9c856e65e63ca11567c6c1c573256443a36d6d
Aysooda Akbari <aysoodaakbariii5515@gmail.com>
2022-10-16 10:23:10 +0330

    Add some styles

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Alert } from 'react-native'
+import React from 'react'
+import { StyleSheet, View,TouchableOpacity,SafeAreaView} from 'react-native'
 import { RNCamera } from 'react-native-camera'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <RNCamera
-      style={{ flex: 1, alignItems: 'center' }}
-      ref={ref => {
-        this.camera = ref
-      }}
-    />
+    <View style={styles.screen}>
+    <SafeAreaView style={styles.saveArea}>
+      <View style={styles.topBar}>
+        <Text style={styles.topBarTitleText}> CamScanner</Text>
+      </View>
+    </SafeAreaView>
+
+    <RNCamera style={styles.rnCamera} />
+    <View style={styles.cameraControl}>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>New QR Scan</Text>
+      </TouchableOpacity>
+    </View>
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  topBar: {
+    height: 50,
+    backgroundColor: '#62d1bc', // green
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topBarTitleText: {
+    color: '#ffffff', // white
+    fontSize: 20,
+  },
+  rnCamera: {
+    flex: 1,
+    width: '94%',
+    alignSelf: 'center',
+  }
+
 });
